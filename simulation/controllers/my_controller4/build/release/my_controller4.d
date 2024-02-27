e0e083d1be09c7a86295af159a18788c5e5aad72
Mikołaj Urbański <01181180@pw.edu.pl>
2024-02-27 20:04:22 +0100

    Saga silników

---
+++ simulation/controllers/my_controller4/build/release/my_controller4.d
@@ -0,0 +1,5 @@
+build/release/my_controller4.o: my_controller4.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h
---
+++ simulation/controllers/my_controller4/my_controller4.c
@@ -0,0 +1,34 @@
+#include <webots/robot.h>
+#include <webots/motor.h>
+
+int main() {
+  wb_robot_init();
+
+  // Pobierz uchwyt do silnika (śmigła)
+  WbDeviceTag propeller1 = wb_robot_get_device("motor_1");//blue
+  WbDeviceTag propeller2 = wb_robot_get_device("motor_2");//yellow
+  WbDeviceTag propeller3 = wb_robot_get_device("motor_3");//green
+  WbDeviceTag propeller4 = wb_robot_get_device("motor_4"); //red
+  
+  // Ustaw silnik w tryb prędkości
+  wb_motor_set_position(propeller1, INFINITY);
+  wb_motor_set_position(propeller2, INFINITY);
+  wb_motor_set_position(propeller3, INFINITY);
+  wb_motor_set_position(propeller4, INFINITY);
+
+
+
+  // Ustaw prędkość obrotową śmigła
+  wb_motor_set_velocity(propeller1, 5.0);
+  wb_motor_set_velocity(propeller2, 5.0);
+  wb_motor_set_velocity(propeller3, 5.0);
+  wb_motor_set_velocity(propeller4, 5.0);  // Ustaw prędkość na 10 radianów na sekundę
+
+  // Pętla główna
+  while (wb_robot_step(64) != -1) {
+    // Możesz dodać dodatkową logikę sterowania tutaj
+  }
+
+  wb_robot_cleanup();
+  return 0;
+}
