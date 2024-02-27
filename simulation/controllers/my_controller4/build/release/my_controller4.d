a5abbbb1432150f6c25ccb7a162744278b8d7133
Marcin Kowalczyk <01169191@pw.edu.pl>
2024-02-27 20:29:18 +0100

    deleted exe files

--- simulation/controllers/my_controller4/build/release/my_controller4.d
+++
@@ -1,5 +0,0 @@
-build/release/my_controller4.o: my_controller4.c \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h
