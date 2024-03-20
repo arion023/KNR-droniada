1dd2550d53951ec0366ccd48b9a91fb956d3eb6a
Michal Lejwoda <michallejwoda@biskupiak.pl>
2024-03-20 19:43:32 +0100

    Cyprian S drone Controller

--- simulation/controllers/my_controller4/build/release/my_controller4.d
+++ simulation/controllers/my_controller4/build/release/my_controller4.d
@@ -1,5 +1,13 @@
 build/release/my_controller4.o: my_controller4.c \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h
+ C:\Users\hyper\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\hyper\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\hyper\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\hyper\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
+ C:\Users\hyper\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Users\hyper\AppData\Local\Programs\Webots/include/controller/c/webots/compass.h \
+ C:\Users\hyper\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
+ C:\Users\hyper\AppData\Local\Programs\Webots/include/controller/c/webots/gyro.h \
+ C:\Users\hyper\AppData\Local\Programs\Webots/include/controller/c/webots/inertial_unit.h \
+ C:\Users\hyper\AppData\Local\Programs\Webots/include/controller/c/webots/keyboard.h \
+ C:\Users\hyper\AppData\Local\Programs\Webots/include/controller/c/webots/led.h \
+ C:\Users\hyper\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h
