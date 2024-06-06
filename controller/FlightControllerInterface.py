dc9d6a3f2e856d3dc099ea3b8d0982625fadb307
Cyprian Stojek <cypsto@gmail.com>
2024-06-06 07:28:20 +0200

    barometr

--- controller/FlightControllerInterface.py
+++ controller/FlightControllerInterface.py
@@ -1,22 +1,39 @@
+class DroneMovement:
+    def __init__(self, interface):
+        self.interface = interface
 
-class FlightControllerInterface:
+    def move_left(self, speed):
+        self.interface.move(-speed, 0, 0)
+        print(f'Move left with speed: {speed}')
 
-    def __init__(self):
-        pass
+    def move_right(self, speed):
+        self.interface.move(speed, 0, 0)
+        print(f'Move right with speed: {speed}')
 
-    def goto_point(self, x,y,z):
-        pass
-    
-    def move(self, x,y,z):
-    #velocities
-        pass
+    def move_forward(self, speed):
+        self.interface.move(0, speed, 0)
+        print(f'Move forward with speed: {speed}')
+
+    def move_back(self, speed):
+        self.interface.move(0, -speed, 0)
+        print(f'Move back with speed: {speed}')
+
+    def move_up(self, speed):
+        self.interface.move(0, 0, speed)
+        print(f'Move up with speed: {speed}')
+
+    def move_down(self, speed):
+        self.interface.move(0, 0, -speed)
+        print(f'Move down with speed: {speed}')
 
     def land(self):
-        pass
+        self.interface.land()
+        print('Landing')
 
-    def rotate(self,angle):
-        pass
-    
-    def get_position(self):
-        pass
-        # return position
+    def start(self):
+        self.interface.start()
+        print('Starting')
+
+    def terminate(self):
+        self.interface.terminate_handler()
+        print('Terminating handler')
